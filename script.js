a177ba05cba3da93dd4bd8a4cbe83d0c5c4b1d6a
HayssousOthmane <jassous.othmane2006@gmail.com>
2026-05-16 16:31:34 +0100

    nihahaha

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ console.log(nom);
 console.log(email);
 console.log(password);
 
+const SavedUser = JSON.parse(localStorage.getItem("user")) || 'Null';
+
+if (SavedUser !== 'Null') {
+    nom.value = SavedUser.name || '';
+    email.value = SavedUser.email || '';
+    password.value = SavedUser.password || '';
+}
+
+const user = {
+    name: nom.value,
+    email: email.value,
+    password: password.value
+};
+
+localStorage.setItem("user", JSON.stringify(user));
+
 submit.addEventListener("click", function(event){
 
     
